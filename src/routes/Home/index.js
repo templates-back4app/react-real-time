e439000fd26e0da619543fba40a649581d1f91b0
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2021-11-30 21:06:46 +0330

    fix component lifecycle with live data from back4app server

--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,16 +1,16 @@
-import {useEffect, useState } from 'react'
-import {useHistory} from 'react-router-dom';
+import { useEffect, useState } from 'react'
+import { useHistory } from 'react-router-dom';
 import Parse from 'parse';
 import './Home.css'
-import {useParseQuery} from '@parse/react';
+import { useParseQuery } from '@parse/react';
 
 
 export default function Home() {
   const [postText, setPostText] = useState('');
   const history = useHistory();
-  
-  const parseQuery = new Parse.Query('Post');
-  parseQuery.descending("createdAt");
+
+  const Character = Parse.Object.extend('Game');
+  const characterQuery = new Parse.Query(Character);
 
   useEffect(() => {
     async function checkUser() {
@@ -22,53 +22,59 @@ export default function Home() {
     }
     checkUser();
   }, []);
-  
 
   const {
-    isLive,
-    isLoading,
-    isSyncing,
-    results,
-    count,
-    error,
-    reload
+    isLive, // Indicates that Parse Live Query is connected
+    isLoading, // Indicates that the initial load is being processed
+    isSyncing, // Indicates that the library is getting the latest data from Parse Server
+    results, // Stores the current results in an array of Parse Objects
+    count, // Stores the current results count
+    error, // Stores any error
+    reload // Function that can be used to reload the data
   } = useParseQuery(
-    parseQuery
+    characterQuery, // The Parse Query to be used
+    {
+      enableLocalDatastore: true, // Enables cache in local datastore (default: true)
+      //cancel web socket conection
+      enableLiveQuery: false // Enables live query for real-time update (default: true)
+    }
   );
 
   const handleSubmitPost = (e) => {
     e.preventDefault();
-    const Post = Parse.Object.extend("Post");
+    const Post = Parse.Object.extend("Game");
     const newPost = new Post();
     newPost.save({
-      text: postText,
+      name: postText,
       authorName: Parse.User.current().get('username'),
     });
-    setPostText("");
+    //get data once after post
+    reload()
+    setPostText(" ");
   };
-  
+
   return (
     <div className="App">
       <header className="app-header">
-      <img className="logo" alt="back4app's logo" src={'https://blog.back4app.com/wp-content/uploads/2019/05/back4app-white-logo-500px.png'} />
+        <img className="logo" alt="back4app's logo" src={'https://blog.back4app.com/wp-content/uploads/2019/05/back4app-white-logo-500px.png'} />
         <h2 className="spacing">parse hooks</h2>
         <span>social network</span>
       </header>
-      
+
       <div className="posts-container">
-      <form onSubmit={handleSubmitPost}className="actions">
-        <textarea value={postText} onChange={event => setPostText(event.currentTarget.value)}/>
-        <button type="submit">post</button>
-      </form>
+        <form onSubmit={handleSubmitPost} className="actions">
+          <textarea  value={postText} onChange={event => setPostText(event.currentTarget.value)} />
+          <button type="submit" onKeyPress={handleSubmitPost}>post</button>
+        </form>
 
 
-      <div className="post-list">
-        {results && results.map((user,index) => (
-        <div className="post" key={index}>
-          <span>{user.get('authorName')}</span>
-          <p>{user.get('text')}</p>
-        </div>))}
-      </div>
+        <div className="post-list">
+          {results && results.map((user, index) => (
+            <div className="post" key={index}>
+              <span>{user.get('authorName')}</span>
+              <p>{user.get('name')}</p>
+            </div>))}
+        </div>
       </div>
     </div>
   );
