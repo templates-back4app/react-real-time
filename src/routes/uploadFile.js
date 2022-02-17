d6ac31be303b20c12b3f054f6d5c21dbbecd669d
EhsanParsania <75175231+EhsanParsania@users.noreply.github.com>
2022-02-18 00:58:03 +0330

    edit typo

--- src/routes/uploadFile.js
+++ src/routes/uploadFile.js
@@ -10,7 +10,7 @@ const getBase64 = (url) => {
 }
 
 
-Parse.Cloud.define('sync-steam-avatar', async (request) => {
+Parse.Cloud.define('save-file', async (request) => {
   const url = 'https://cdn.akamai.steamstatic.com/steamcommunity/public/images/avatars/c5/c5d56249ee5d28a07db4ac9f7f60af961fab5426_full.jpg'
   const object = request.user
   const base64 = await getBase64(url)
